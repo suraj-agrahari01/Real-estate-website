c13af94be8d9124ae98f09dbb48650ea03f09fbb
Suraj-agrahari <Tha075bei043@tcioe.edu.np>
2023-07-06 20:57:18 +0545

    adding javascrpit

--- scrpit.js
+++ scrpit.js
@@ -3,4 +3,12 @@ const fullImgOverlay = document.querySelector('.full-img-overlay');
 const closeBtn = document.querySelector('.close-btn');
 
 //
-
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
